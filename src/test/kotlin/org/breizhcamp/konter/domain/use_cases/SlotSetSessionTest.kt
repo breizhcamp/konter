d5150b85f90b7e624e5556b7fa5086dc8cc0f400
claire-lucas <claire.lucas.perso@gmail.com>
2024-06-10 14:19:37 +0200

    SlotSetSession tested

--- src/test/kotlin/org/breizhcamp/konter/domain/use_cases/SlotSetSessionTest.kt
+++ src/test/kotlin/org/breizhcamp/konter/domain/use_cases/SlotSetSessionTest.kt
@@ -1,12 +1,20 @@
 package org.breizhcamp.konter.domain.use_cases
 
 import com.ninjasquad.springmockk.MockkBean
+import io.mockk.every
+import io.mockk.verify
+import org.breizhcamp.konter.domain.entities.Session
 import org.breizhcamp.konter.domain.use_cases.ports.SessionPort
+import org.breizhcamp.konter.testUtils.SessionGen
+import org.breizhcamp.konter.testUtils.generateRandomHexString
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.test.context.junit.jupiter.SpringExtension
+import java.util.*
 
 @ExtendWith(SpringExtension::class)
 @WebMvcTest(SlotSetSession::class)
@@ -18,11 +26,30 @@ class SlotSetSessionTest {
     @Autowired
     private lateinit var slotSetSession: SlotSetSession
 
-    @Test
-    fun setById() {
+    private lateinit var session: Session
+
+    @BeforeEach
+    fun setUp() {
+        session = SessionGen().generateOne()
     }
 
     @Test
-    fun setByBarcode() {
+    fun `setById should call Port with its inputs and return the result`() {
+        val slotId = UUID.randomUUID()
+        every { sessionPort.setSlotById(session.id, slotId) } returns session
+
+        assertEquals(session, slotSetSession.setById(session.id, slotId))
+
+        verify { sessionPort.setSlotById(session.id, slotId) }
+    }
+
+    @Test
+    fun `setByBarcode should call Port with its inputs and return the result`() {
+        val barcode = generateRandomHexString()
+        every { sessionPort.setSlotByBarcode(session.id, barcode) } returns session
+
+        assertEquals(session, slotSetSession.setByBarcode(session.id, barcode))
+
+        verify { sessionPort.setSlotByBarcode(session.id, barcode) }
     }
 }
