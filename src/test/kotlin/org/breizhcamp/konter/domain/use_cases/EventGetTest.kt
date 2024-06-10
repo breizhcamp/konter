08c596ab91a79cf565e008897daef7d985a865b7
claire-lucas <claire.lucas.perso@gmail.com>
2024-06-10 12:18:23 +0200

    EventGet tested

--- src/test/kotlin/org/breizhcamp/konter/domain/use_cases/EventGetTest.kt
+++ src/test/kotlin/org/breizhcamp/konter/domain/use_cases/EventGetTest.kt
@@ -1,7 +1,11 @@
 package org.breizhcamp.konter.domain.use_cases
 
 import com.ninjasquad.springmockk.MockkBean
+import io.mockk.every
+import io.mockk.verify
 import org.breizhcamp.konter.domain.use_cases.ports.EventPort
+import org.breizhcamp.konter.testUtils.EventGen
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
@@ -19,6 +23,12 @@ class EventGetTest {
     private lateinit var eventGet: EventGet
 
     @Test
-    fun getById() {
+    fun `getById should call port with its input and return the result`() {
+        val event = EventGen().generateOne()
+        every { eventPort.getById(event.id) } returns event
+
+        assertEquals(event, eventGet.getById(event.id))
+
+        verify { eventPort.getById(event.id) }
     }
 }
